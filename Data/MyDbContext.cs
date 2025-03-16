5eb7424395a481d0407b74e149dbbbc27c019cdc
Andan <141715477+AdanCC21@users.noreply.github.com>
2025-03-16 15:15:51 -0700

    Agregar archivos de proyecto.

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options){}
+
+        public DbSet<Pokemon> MyPokemonEntity { get; set; }
+    }
+}
---
+++ Presentation/Migrations/MyDb/20250316214950_InitialMig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Presentation.Migrations.MyDb
+{
+    /// <inheritdoc />
+    public partial class InitialMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyPokemonEntity",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyPokemonEntity", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyPokemonEntity");
+        }
+    }
+}
